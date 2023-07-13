c5ad5672f290c191cb710ed47d669d138027d1f0
Jessmor2 <135068935+Jessmor2@users.noreply.github.com>
2023-07-13 18:33:11 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ["the", "our", "my", "everyones"];
+let adjective = ["best", "greatest", "coolest", "largest"];
+let noun = ["knives", "footballs", "bathroom", "pumpkin"];
+let ext = [".com", ".net", ".us", ".gov"];
+
+let getDomain = () => {
+  for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
+    for (
+      let adjectiveIndex = 0;
+      adjectiveIndex < adjective.length;
+      adjectiveIndex++
+    )
+      for (let nounIndex = 0; nounIndex < noun.length; nounIndex++)
+        for (let extIndex = 0; extIndex < ext.length; extIndex++)
+          console.log(
+            pronoun[pronounIndex] +
+              adjective[adjectiveIndex] +
+              noun[nounIndex] +
+              ext[extIndex]
+          );
+  }
 };
+let domainName = getDomain();
